f8f7d324dcfa04d51dd8c2bbb45687b96785e1aa
Smriti <smriti.sivasang@gmail.com>
2023-10-08 18:36:13 +0530

    Login Component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
+import { FormControl} from '@angular/forms';
+import { FormGroup} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  constructor(){}
+export class AppComponent implements OnInit {
+  constructor(
+    private formBuilder: FormBuilder
+  ){}
+  
   headerTitle = "Login Form";
   buttonTitle = "Submit";
+
+  loginForm!: FormGroup;
+
+  ngOnInit():void{
+    console.log('onit called');
+    this.loginForm = this.formBuilder.group({
+      username: ['Smriti'],
+      password: new FormControl('')
+    });
+  }
+  login()
+  {
+    console.log(this.loginForm.value)
+  }
 }
